6d0be0553e64f1a43d7b8249a8ce5c17c2a0d812
Josh Peters <petersjt@msoe.edu>
2017-07-11 19:54:42 -0500

    more on track (yay)

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-var request = require('request')
-var hyperfeed = require('hyperfeed')
-var hyperdrive = require('hyperdrive')
-var swarm = require('hyperdiscovery')
+import request from '/request';
+import hyperfeed from '/hyperfeed';
+import hyperdrive from '/hyperdrive';
+import hyperdiscovery from '/hyperdiscovery';
+
+function testThings() {
+
+  dateStr = "Date: " + new Date();
+  document.getElementById('tester').innerHTML = dateStr;
+  console.log(dateStr);
+}
+
+function otherThing(){
+
+  console.log('[wip]')
+}
