e4fd1d3ea0f2109804e0783bbd6ebdafe6102907
Josh Peters <petersjt@msoe.edu>
2017-07-11 20:45:34 -0500

    more shiat

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,14 @@
 // import hyperdrive from '/hyperdrive';
 // import hyperdiscovery from '/hyperdiscovery';
 
+import debug from 'debug';
+const log = debug('app:log');
+
+// Enable the logger.
+debug.enable('*');
+log('Logging is enabled!');
+
+
 function testThings() {
 
   dateStr = "Date: " + new Date();
@@ -12,5 +20,11 @@ function testThings() {
 
 function otherThing(){
 
-  console.log('[wip]')
+  console.log('[wip]');
 }
+
+function sayHi(name){
+  return "Hi " + name + ", it's currently " + new Date();
+}
+
+const result1 = sayHi('sam');
